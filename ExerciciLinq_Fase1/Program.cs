f2b1d5b542a5066683146038c032c04db0f5e49d
quimaruanomed <jruanomed@gmail.com>
2020-12-07 07:00:15 +0100

    first commit

---
+++ ExerciciLinq_Fase1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ExerciciLinq_Fase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numFaseI = { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4};
+            
+            IEnumerable<int> numP= from num in numFaseI where num % 2 == 0 select num; // Consulta para seleccionar los numeros pares
+            Console.WriteLine("Numeros pares:\n");
+            foreach (int n in numP) // Mostramos los resultados de los numeros pares que contiene el Array
+            {
+                Console.WriteLine(n);
+            }
+            
+        }
+    }
+}
